2eafcd8de3f9cdbfaabe7a215dc2606724f003e5
Lisa Dafny <lisadafny.tech@gmail.com>
2024-02-13 21:59:03 -0300

    feature: deploy erc20 contract with form data

--- util.js
+++ util.js
@@ -1,24 +1,30 @@
+const btnName = $("#inputTokenName");
+const btnSymbol = $("#inputTokenSymbol");
+const btnDecimals = $("#inputDecimals");
 let signer = null;
-
 let provider;
-if (window.ethereum == null) {
+let factory;
 
-    // If MetaMask is not installed, we use the default provider,
-    // which is backed by a variety of third-party services (such
-    // as INFURA). They do not have private keys installed,
-    // so they only have read-only access
-    console.log("MetaMask not installed; using read-only defaults")
-    provider = ethers.getDefaultProvider()
+async function start() {
+    if (window.ethereum == null) {
+        console.log("MetaMask not installed");
+        provider = ethers.getDefaultProvider();
 
-} else {
-
-    // Connect to the MetaMask EIP-1193 object. This is a standard
-    // protocol that allows Ethers access to make all read-only
-    // requests through MetaMask.
-    provider = new ethers.BrowserProvider(window.ethereum)
-
-    // It also provides an opportunity to request access to write
-    // operations, which will be performed by the private key
-    // that MetaMask manages for the user.
-    signer = await provider.getSigner();
+    } else {
+        provider = new ethers.BrowserProvider(window.ethereum);
+        signer = await provider.getSigner();
+        factory = new ethers.ContractFactory(abi, bytecode, signer);
+    }
 }
+$("#btnSubmit").on('submit', async () => {
+    const name = btnName.val();
+    const symbol = btnSymbol.val();
+    const decimals = btnDecimals.val();
+
+    const contract = await factory.deploy(name, symbol, decimals);
+
+    console.log("contract address: ", contract.address)
+
+    await contract.deployTransaction.wait();
+});
+start();
