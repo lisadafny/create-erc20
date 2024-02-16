76ad639f601ad4544c882a7186c20344e1ef61b3
Lisa Dafny <lisadafny.tech@gmail.com>
2024-02-16 00:29:56 -0300

    feature: call contract functions

--- util.js
+++ util.js
@@ -1,30 +1,34 @@
-const btnName = $("#inputTokenName");
-const btnSymbol = $("#inputTokenSymbol");
-const btnDecimals = $("#inputDecimals");
 let signer = null;
 let provider;
 let factory;
 let contract;
+let funcContract;
 
 async function start() {
     if (window.ethereum == null) {
         console.log("MetaMask not installed");
         provider = ethers.getDefaultProvider();
         $("#sectionCreateContract").addClass("d-none");
-    } else {
-        provider = new ethers.BrowserProvider(window.ethereum);
-        signer = await provider.getSigner();
-        factory = new ethers.ContractFactory(abi, bytecode, signer);
-        $("#navAddress").html("Welcome " + signer.address);
+        return;
     }
+
+    provider = new ethers.BrowserProvider(window.ethereum);
+    signer = await provider.getSigner();
+    factory = new ethers.ContractFactory(abi, bytecode, signer);
+    $("#navAddress").html("Welcome " + signer.address);
+
     $("#formContract").on('submit', deploy);
+    $("#formAction").on('submit', callContract);
+    $(".contract-action").on('click', changeToContractCallingSection);
+
 }
+
 async function deploy(event) {
     try {
         event.preventDefault();
-        const name = btnName.val();
-        const symbol = btnSymbol.val();
-        const decimals = btnDecimals.val();
+        const name = $("#inputTokenName").val();
+        const symbol = $("#inputTokenSymbol").val();
+        const decimals = $("#inputDecimals").val();
 
         contract = await factory.deploy(name, symbol, decimals);
 
@@ -38,14 +42,52 @@ async function deploy(event) {
         $("#contractHash").html(contract.target);
     } catch (error) {
         $("#modalInfo").modal("show");
-        if(error.reason){
+        if (error.reason) {
             $(".modal-title").html("Error: " + error.reason);
         }
-        if(error.shortMessage){
+        if (error.shortMessage) {
             $(".modal-body p").html(error.shortMessage);
             return;
         }
         $(".modal-body p").html(error);
     }
 }
+
+function changeToContractCallingSection(event) {
+    $("#contractCalling h2").html(event.currentTarget.innerHTML);
+    $("#contractCalling").removeClass("d-none");
+
+}
+
+async function callContract(event) {
+    try {
+        event.preventDefault();
+        const addressValue = $("#inputAddress").val();
+        const amountValue = $("#inputAmount").val();
+
+        if (!contract || !addressValue || !amountValue) {
+            $("#modalInfo").modal("show");
+            return;
+        }
+
+        let funcTxt = $(".btn-check:checked")[0].id;
+        funcContract = await contract.getFunction(funcTxt);
+
+        const tx = await funcContract.send(addressValue, amountValue);
+
+        const txReceipt = await tx.wait();
+        console.log("txReceipt: ", txReceipt);
+    } catch (error) {
+        $("#modalInfo").modal("show");
+        if (error.reason) {
+            $(".modal-title").html("Error: " + error.reason);
+        }
+        if (error.shortMessage) {
+            $(".modal-body p").html(error.shortMessage);
+            return;
+        }
+        $(".modal-body p").html(error);
+    }
+}
+
 start();
